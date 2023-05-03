27a4ac3185b40fab1a7bf90203452bf36e4a5e10
Khaled-Ballour <ballourk@gmail.com>
2023-05-03 22:58:00 +0300

    initite the server file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routers/tourRouter');
 const userRouter = require('./routers/userRouter');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -9,7 +10,4 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`the server running on port: ${port}...`);
-});
+module.exports = app;
