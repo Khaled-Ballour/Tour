ed86e8c98f4429fb82ddc3e2d028819573056ab2
Khaled-Ballour <ballourk@gmail.com>
2023-05-03 22:38:55 +0300

    building the initial tour router

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const tourRouter = require('./routers/tourRouter');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/api/v1/tours', tourRouter);
 
 app.get('/api/v1/', (req, res) => {
   res.json({ status: 200, data: 'Hello' });
