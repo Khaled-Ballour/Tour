24792cceda5e99a3ec5edb4a787a61128d59a1cc
Khaled-Ballour <ballourk@gmail.com>
2023-05-03 22:30:00 +0300

    -

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/api/v1/', (req, res) => {
+  res.json({ status: 200, data: 'Hello' });
+});
 
 const port = 3000;
 app.listen(port, () => {
