beed3bc4bee9dd42d6ef091de67b88896f38e0c7
Khaled-Ballour <ballourk@gmail.com>
2023-05-10 12:36:07 +0300

    connecting with remote database

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
+
 const app = require('./app');
 const port = 3000;
+
+const DB_options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+const DB_uri = process.env.DB.replace(
+  '<password>',
+  process.env.DB_PASSWORD,
+  DB_options
+);
+
+mongoose.connect(DB_uri).then((con) => {});
+
 app.listen(port, () => {
   console.log(`the server running on port: ${port}...`);
 });
