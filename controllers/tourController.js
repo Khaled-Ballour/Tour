38e2baee3b9eea49f7920cd25bee63d2da99979f
Khaled-Ballour <ballourk@gmail.com>
2023-05-10 18:10:27 +0300

    Tour-crud

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,11 +1,29 @@
-exports.getAllTours = (req, res, next) => {
-  res.json({ status: 200, data: 'Tours' });
+const Tour = require('../Models/tourModel');
+
+exports.getAllTours = async (req, res, next) => {
+  const tours = await Tour.find();
+  res.json({ status: 'success', tours });
 };
 
-exports.createTour = (req, res, next) => {};
+exports.createTour = async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.json({ status: 'success', newTour });
+};
 
-exports.updateTour = (req, res, next) => {};
+exports.updateTour = async (req, res, next) => {
+  const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.json({ status: 'success', updatedTour });
+};
 
-exports.deleteTour = (req, res, next) => {};
+exports.deleteTour = async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  res.json({ status: 'success', data: null });
+};
 
-exports.getTour = (req, res, next) => {};
+exports.getTour = async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+  res.json({ status: 'success', tour });
+};
