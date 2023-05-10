37347b6b51ba009ae779f54026e8ed232eb7dcc5
Khaled-Ballour <ballourk@gmail.com>
2023-05-10 19:45:12 +0300

    import data

--- dev-data/data/import-data-dev.js
+++ dev-data/data/import-data-dev.js
@@ -1,15 +1,33 @@
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../../.env' });
+const fs = require('fs');
 const Tour = require('../../Models/tourModel');
+const DB_connection = require('../../utils/DB-connection');
 
-const DB_options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-};
-const DB_uri = process.env.DB.replace(
-  '<password>',
-  process.env.DB_PASSWORD,
-  DB_options
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
 
-mongoose.connect(DB_uri).then((con) => {});
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded!');
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted!');
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+DB_connection();
+if (process.argv[2] === '--import') importData();
+if (process.argv[2] === '--delete') deleteData();
