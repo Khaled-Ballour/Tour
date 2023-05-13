5fc8efe7aa1ff79265db9f39053d48ce74c055f5
Khaled-Ballour <ballourk@gmail.com>
2023-05-13 10:00:13 +0300

    Route is not found

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({
+      status: 'fail',
+      message: `The route ${req.originalUrl} is not found`,
+    });
+});
 
 module.exports = app;
