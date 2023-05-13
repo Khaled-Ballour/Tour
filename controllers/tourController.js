11962dae9148ec9b3f2cd61a92701c37e3ed5ff5
Khaled-Ballour <ballourk@gmail.com>
2023-05-13 10:48:14 +0300

    catchAsync handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,35 +1,36 @@
 const Tour = require('../Models/tourModel');
 const FeatureAPI = require('../utils/FeatureAPI');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getAllTours = async (req, res, next) => {
+exports.getAllTours = catchAsync(async (req, res, next) => {
   const featureAPI = new FeatureAPI(Tour.find(), JSON.stringify(req.query));
   featureAPI.flitter().sort().select().pagination();
   const tours = await featureAPI.query;
   res.json({ status: 'success', size: tours.length, tours });
-};
+});
 
-exports.createTour = async (req, res, next) => {
+exports.createTour = catchAsync(async (req, res, next) => {
   const newTour = await Tour.create(req.body);
   res.json({ status: 'success', newTour });
-};
+});
 
-exports.updateTour = async (req, res, next) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
   const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
   res.json({ status: 'success', updatedTour });
-};
+});
 
-exports.deleteTour = async (req, res, next) => {
+exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
   res.json({ status: 'success', data: null });
-};
+});
 
-exports.getTour = async (req, res, next) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   res.json({ status: 'success', tour });
-};
+});
 
 exports.getTourStats = async (req, res, next) => {
   try {
