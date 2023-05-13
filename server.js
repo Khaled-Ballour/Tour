0e4479fd5b006b4aef2211c95c2f4186bc9f81e6
Khaled-Ballour <ballourk@gmail.com>
2023-05-13 15:34:06 +0300

    Internal excwption

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  exit(1);
+});
+
 const DB_connection = require('./utils/DB-connection');
 const app = require('./app');
 const port = 3000;
 
-DB_connection().then(() => {
-  app.listen(port, () => {
-    console.log(`the server running on port: ${port}...`);
-  });
+const server = app.listen(port, () => {
+  console.log(`the server running on port: ${port}...`);
+});
+DB_connection().then(console.log('Connected to the database'));
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
 });
