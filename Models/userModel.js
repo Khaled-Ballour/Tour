f7bb67dea757cfc9b411a3ecbe3d1003a7cc05a0
Khaled-Ballour <ballourk@gmail.com>
2023-05-15 19:27:19 +0300

    reset password after forgeting it

--- Models/userModel.js
+++ Models/userModel.js
@@ -45,8 +45,13 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
-  this.passwordChangedAt = Date.now() - 1000;
   this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
   next();
 });
 
