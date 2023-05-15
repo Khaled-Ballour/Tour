7beab2e6128adfb215f585faa706fc7681b1a827
Khaled-Ballour <ballourk@gmail.com>
2023-05-15 19:37:57 +0300

    update the user password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -121,3 +121,13 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   const token = signToken(user._id);
   res.status(200).json({ status: 'success', token });
 });
+
+exports.updateUserPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('your current password is wrong', 401));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+});
