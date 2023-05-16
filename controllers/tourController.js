53c8ee119bfed92b32a295b31f961935245108b5
Khaled-Ballour <ballourk@gmail.com>
2023-05-16 12:07:17 +0300

    virtual populating reviews in a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,7 +32,7 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate('reviews');
   if (!tour) return next(new AppError(`No tour found with that ID`, 404));
   res.json({ status: 'success', tour });
 });
