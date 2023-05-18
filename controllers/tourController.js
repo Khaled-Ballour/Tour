4b5cb0261a8e621bf5783c469475c43ad060dc60
Khaled-Ballour <ballourk@gmail.com>
2023-05-18 16:46:10 +0300

    distance within function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,6 +11,28 @@ exports.createTour = handlerFactory.createOne(Tour);
 exports.updateTour = handlerFactory.updateOne(Tour);
 exports.deleteTour = handlerFactory.deleteOne(Tour);
 
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
+  }
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+  res.status(200).json({
+    status: 'success',
+    size: tours.length,
+    data: tours,
+  });
+});
+
 exports.getTourStats = async (req, res, next) => {
   try {
     const stats = await Tour.aggregate([
